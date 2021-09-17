7564338d2d4cad9236a6d5a5ba84080679ea53e1
rubensb02 <rubensb02@gmail.com>
2021-09-17 18:15:06 +0200

    correccion

--- error.java
+++ error.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 public class error{
 	public static void main(String[] args) {
-		Scanner sc= new Scanner;
+		Scanner sc = new Scanner(System.in);
 		int num;
 		System.out.println("Introduce un numero: ");
 		num=sc.nextInt();
 			
-		System.out.println("Has introducido: "+num);
+		System.out.println("Has introducido: " + num);
 		
 		
 	}
