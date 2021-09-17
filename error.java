f94c1299510cf88fbd13f4da43cef93150bded19
OriekB <posea.ben@gmail.com>
2021-09-17 18:03:11 +0200

    vers 2

--- error.java
+++ error.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
-public static void main(String[] args) {
-    Scanner sc= new Scanner;
-    int num;
-    System.out.println("Introduce un numero: ");
-    num=sc.nextInt();
-    
-    System.out.println("Has introducido: "+num);
-    
+public class error{
+	public static void main(String[] args) {
+		Scanner sc= new Scanner;
+		int num;
+		System.out.println("Introduce un numero: ");
+		num=sc.nextInt();
+			
+		System.out.println("Has introducido: "+num);
+		
+		
+	}
 }
